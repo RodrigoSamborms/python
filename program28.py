58a4aa94ca2eb0d98193ef51f9e082b4e16a2231
RodrigoSamborms <your-correct-email@example.com>
2026-03-31 04:42:07 -0600

    casi completo el ejercicio 28

--- program28.py
+++ program28.py
@@ -10,10 +10,15 @@ import sys
 
 if __name__ == '__main__':
     N = int(input().strip())
-
+    tabla = []
     for N_itr in range(N):
         first_multiple_input = input().rstrip().split()
-
         firstName = first_multiple_input[0]
-
         emailID = first_multiple_input[1]
+        tabla.append((firstName, emailID))
+        
+    for i in range(N):
+        print (firstName)
+        #print (emailID)
+    
+            
